c53e7323c1080c20d9388516b66e8836f2f9d0d4
Brad Alvers <flowchart@hub.fxn.io>
2026-03-13 19:34:14 +0000

    new

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
+  base: "/dashboard/",
   plugins: [react(), cloudflare()],
   server: {
     allowedHosts: true,
